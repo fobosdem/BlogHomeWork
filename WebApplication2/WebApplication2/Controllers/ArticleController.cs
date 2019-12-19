da79af17c219d7984d5fca68a505e6dd3d51d390
fobosdem <fobosdem@gmail.com>
2019-12-19 18:20:28 +0200

    adding home work

---
+++ WebApplication2/WebApplication2/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+			List<Article> articls = new List<Article>()
+			{
+				new Article(){ Id = 1, Title ="FirstTitle", date = new DateTime(), Comment="First Article Comment", Text = "Text of first article" },
+				new Article(){ Id = 2, Title ="SecondTitle", date = new DateTime(), Comment="Second Article Comment", Text = "Text of Second article" }
+			};
+
+            return View(articls);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+	public class Article
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Text { get; set; }
+		public DateTime date { get; set; }
+		public string Comment { get; set; }
+	}
+}
